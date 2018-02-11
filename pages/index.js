e82750be55b07cf4bccbe5a64c16e6fd4b3a2950
Honza Sušek <honza@susek.cz>
2018-02-11 12:04:23 +0100

    Add react webcam

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,58 @@
 import React from 'react'
+import isBrowser from 'lib/isBrowser'
+import updateState from 'lib/updateState'
+import { compose, withHandlers, withStateHandlers } from 'recompose'
 
-export default () => (
-  <div>You have succesfully installed nextjs-boilerplate.</div>
-)
+let webcamInstance
+
+const setRef = instance => {
+  webcamInstance = instance
+}
+
+export default compose(
+  withStateHandlers(
+    {
+      imageSrc: null,
+      videoWidth: 640,
+      videoHeight: 480,
+    },
+    {
+      setImageSrc: ({ imageSrc }) => value =>
+        updateState('imageSrc', imageSrc, value),
+      setVideoDimensions: ({ videoWidth, videoHeight }) => (width, height) => ({
+        ...updateState('videoWidth', videoWidth, width),
+        ...updateState('videoHeight', videoHeight, height),
+      }),
+    }
+  ),
+  withHandlers({
+    handleMetaDataLoad: () => () => {
+      webcamInstance.videoElement.width = webcamInstance.videoElement.videoWidth
+      webcamInstance.videoElement.height =
+        webcamInstance.videoElement.videoHeight
+    },
+  }),
+  withHandlers({
+    capture: ({ setImageSrc }) => () => {
+      setImageSrc(webcamInstance.getScreenshot())
+    },
+    handleUserMedia: ({ handleMetaDataLoad }) => () => {
+      webcamInstance.videoElement.addEventListener(
+        'loadedmetadata',
+        handleMetaDataLoad
+      )
+    },
+  })
+)(({ capture, handleUserMedia, imageSrc }) => {
+  if (!isBrowser) return <div />
+
+  const Webcam = require('@cliener/react-webcam').default
+
+  return (
+    <div>
+      <Webcam onUserMedia={handleUserMedia} ref={setRef} />
+      <button onClick={capture}>Capture photo</button>
+      {imageSrc && <img alt="Captured" src={imageSrc} />}
+    </div>
+  )
+})
